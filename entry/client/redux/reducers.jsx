0ae577ed3660ad24daa731bff3350a455dfd7645
Stephen Wu <stefan.wu@gmail.com>
2015-12-11 11:29:57 -0800

    built out homepage

--- entry/client/redux/reducers.jsx
+++ entry/client/redux/reducers.jsx
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
 import { routeReducer } from 'redux-simple-router'
 
+import Home from 'main_Home/client/components/Reducers.jsx'
 import Presenter from 'main_Presenter/client/components/Reducers.jsx'
 
 // add reducers here
 export default combineReducers({
   routing: routeReducer,
+  Home: Home,
   previews: Presenter
 })
--- modules/sub_Login/client/index.jsx
+++ modules/sub_Login/client/index.jsx
@@ -1,11 +1,12 @@
 /*
   This is the entry point. Export a react component here.
 */
-
 import React, { Component } from 'react';
 
 export default class Login extends Component {
   componentDidMount() {
+    // console.log(Template.loginButtons.toString());
+
     // Use Meteor Blaze to render login buttons
     this.view = Blaze.render(Template.loginButtons,
       React.findDOMNode(this.refs.container));
