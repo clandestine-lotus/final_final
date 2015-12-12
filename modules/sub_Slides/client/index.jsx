7a515aad5cdb92268de7e9c2640788d64db511cb
Kim Ha <kimberlygha@gmail.com>
2015-12-11 18:42:16 -0800

    Added next and prev button, link to pick a presentation

--- modules/sub_Slides/client/index.jsx
+++ modules/sub_Slides/client/index.jsx
@@ -3,6 +3,7 @@
 */
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { Link } from 'react-router'
 // import Presentations from '../globals/collections'
 import * as actionCreator from './actions'
 import Presentations from '../../main_SelectPresentation/globals/collections'
@@ -11,37 +12,25 @@ export default class Slides extends Component {
   constructor() {
     super();
     this.slideMaker = this.slideMaker.bind(this);
-    this.changeSlide = this.changeSlide.bind(this);
   }
 
-  componentDidMount () {
-    window.addEventListener('onKeyPress', this.changeSlide);
-  }
-
-  componentWillUnmount () {
-    window.removeEventListener('onKeyPress', this.changeSlide);
-  }
-
-  changeSlide (event) { 
-    console.log('changing')
-    this.props.nextSlide()
-    // if(event.keyCode === 39){
-    //   this.props.nextSlide()
-    // }
-    // if(event.keyCode === 37){
-    //   this.props.prevSlide();
-    // }
-  }
 
   slideMaker () {
-    let presentation = Presentations.findOne({gid: this.props.presentation})
-    console.log(presentation)
-    let svgs = presentation.svgs
-    return {__html: svgs[this.props.index]}
+    if(this.props.presentation){
+      let presentation = Presentations.findOne({gid: this.props.presentation})
+      console.log(presentation)
+      let svgs = presentation.svgs
+      return {__html: svgs[this.props.index]}
+    }
   }
 
   render () {
-    return <div className="slide" onClick={this.changeSlide} dangerouslySetInnerHTML={this.slideMaker()}></div>
+    return (
+    <div>
+    {this.props.presentation ? <div className="slide" dangerouslySetInnerHTML={this.slideMaker()}></div> :
+      <Link to = "/selectpresentation" >Pick a Presentation</Link> }
+    <div><button onClick={this.props.prevSlide}>prev</button><button onClick={this.props.nextSlide}>next</button></div></div>
+    )
   }
 }
 
