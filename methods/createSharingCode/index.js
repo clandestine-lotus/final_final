13028a92a787596026889c257c6a010836524ec7
Bjarke Staun-Olsen <bjarkestaun@gmail.com>
2015-12-14 22:20:48 -0800

    random code generator no longer allows dublicate

--- methods/createSharingCode/index.js
+++ methods/createSharingCode/index.js
@@ -1,11 +1,23 @@
 const randomString = Meteor.npmRequire('random-string')
 
-// method for creating a new presentation in database with svg elements
-export default function (length) {
-  // change privacy setting of the presentation to public
+// keep object with already used strings to avoid dublicates
+// TODO: refactor to do this in a less stupid way
+var usedStrings = {}
+
+var getRandomString = function (length) {
   return randomString({
     length: length,
     numeric: true,
     letters: true
-  })
+  });
+}
+
+export default function (length) {
+  var result;
+  while (!result) {
+    result = getRandomString(length);
+    result = !usedStrings[result] ? result : undefined;
+  }
+  usedStrings[result] = true;
+  return result;
 }
