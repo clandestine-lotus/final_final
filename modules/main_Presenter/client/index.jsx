c52aa9c9a6bfabf53b6dcf96f9e8efa01616c160
Stephen Wu <stefan.wu@gmail.com>
2015-12-17 16:41:36 -0800

    slides inc/dec properly in main_presenter

--- modules/main_Presenter/client/index.jsx
+++ modules/main_Presenter/client/index.jsx
@@ -1,15 +1,12 @@
 /*
   This is the entry point. Export a react component here.
 */
-import React, { Component } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router'
 
 import * as PresenterActions from './components/PresenterActions.jsx'
 
-import { bindActionCreators, createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './reducers';
 import Slides from 'sub_Slides/client/index'
 import SidebarView from 'sub_SlideSideBar/client/index'
 import Code from 'sub_SharingCode/client/index'
@@ -19,22 +16,12 @@ import Presentation from 'db/Presentations'
 
 let Presenter = React.createClass({
   componentDidMount() {
-    console.log(this.props);
-    let presentation = Presentation.findOne({gid: this.props.params.gid})
-    let user = Meteor.user()._id;
+    const context = this
 
-    let link = this.props.data.link;
-    let gid = this.props.data.gid;
-
-    Meteor.call('createPresentation', link, user, gid, function (err, result) {
-      if (err) {
-        console.error('from preview ', err);
+    Presentation.find({gid: this.props.presentation}).observe({
+      changed: function (id, doc) {
+        context.props.setIndex(doc.index);
       }
-
-      console.log('success!', this.props.params.gid);
-
-      // react.props.setPresentation(gid);
-      // window.open('/projector/' + gid);
     })
   },
 
