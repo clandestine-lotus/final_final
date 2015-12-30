9de74593e49fcba75037b7cdc4241ec45cff1483
Ying Hao Chan <yinghao@outlook.com>
2015-12-30 11:01:48 -0800

    added dev viewer of minimongo collections

--- entry/client/entry.js
+++ entry/client/entry.js
@@ -4,6 +4,12 @@ import './root';
 // set scope for google auth to include drive access
 var scopes = ['https://www.googleapis.com/auth/drive'];
 
+// use ctrl+m to view these collections on the client
+if (process.env.NODE_ENV !== 'production'){
+  Package['meteortoys:toykit'].MeteorToysDict.set('Mongol', 
+    {collections: ['codes', 'decks', 'posts', 'shows'] })
+}
+
 Accounts.ui.config({
   'passwordSignupFields': 'USERNAME_ONLY',
   'requestPermissions': { 'google': scopes },
