9cc6d796af60ec433ce5a6be474a08c6a789dc65
Kim Ha <kimberlygha@gmail.com>
2015-12-31 14:23:21 -0800

    Display viewer count in state

--- modules/dux/audience/index.jsx
+++ modules/dux/audience/index.jsx
@@ -1,12 +1,14 @@
 import {Map, List} from 'immutable'
 
 import Audience from 'db/Audience.js'
+import Shows from 'db/Shows'
 
 //////////////////
 // ACTION TYPES //
 //////////////////
 
 const SET_AUDIENCE = 'SET_AUDIENCE'
+const SET_VIEWERS = 'SET_VIEWERS'
 
 ////////////////////////
 // INTERNAL FUNCTIONS //
@@ -20,17 +22,29 @@ const setAudience = function (audience) {
   }
 }
 
+const setViewers = function (viewers) {
+  return { 
+    type: SET_VIEWERS,
+    payload: viewers
+  }
+}
+
 ///////////////////////
 // TRACKER FUNCTIONS //
 ///////////////////////
 
 export function trackAudience (show) {
   return Tracker.autorun(function () {
+    const {dispatch} = require('../store.js')
     Meteor.subscribe('audience', show)
+    Meteor.subscribe('show', show)
     let audience = Audience.find({presentation: show}).fetch()
     if (audience.length > 0) {
-      const {dispatch} = require('../store.js')
       dispatch(setAudience(audience))
+    }
+    let showing = Shows.findOne({_id: show})
+    if (showing && showing.viewers > -1) {
+      dispatch(setViewers(showing.viewers))
     }
   })
 }
@@ -61,12 +75,15 @@ export function removeViewer (show, user){
 
 const initial = Map({
   audience: List(),
+  viewers: 0
 })
 
 export default function reducer(state = initial, action) {
   switch (action.type) {
   case 'SET_AUDIENCE':
     return state.set('audience', List(action.payload));
+  case 'SET_VIEWERS':
+    return state.set('viewers', action.payload)
   default:
     return state;
   }
